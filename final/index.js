00ed87cf53c9ef0c58a050478aa60fddf5936af8
junhyung0710 <junhyung0710@gmail.com>
2022-11-24 22:04:11 -0800

    stage-set

---
+++ final/index.js
@@ -0,0 +1,22 @@
+window.onload = function WindowLoad(event) {
+    new Freezeframe('.freezeframe1', {
+        trigger: 'hover',
+        overlay: false
+      });
+    new Freezeframe('.freezeframe2', {
+    trigger: 'hover',
+    overlay: false
+    });
+    new Freezeframe('.freezeframe3', {
+        trigger: 'hover',
+        overlay: false});
+    new Freezeframe('.freezeframe4', {
+        trigger: 'hover',
+        overlay: false
+    });
+    new Freezeframe('.freezeframe5', {
+        trigger: 'hover',
+        overlay: false
+    });
+
+}
